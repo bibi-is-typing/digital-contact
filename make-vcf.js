ce0be4b620c6f1738f2e38cc11cbb7a9f6892cf0
hanvy072 <072hanvy@gmail.com>
2025-08-14 22:12:28 +0900

    chore: add hanbi.vcf and GitHub Pages setup for contact sharing

---
+++ make-vcf.js
@@ -0,0 +1,20 @@
+// make-vcf.js
+const fs = require('fs');
+const vCardsJS = require('vcards-js');
+
+const v = vCardsJS();
+v.version = '3.0';
+v.firstName = '한비';
+v.lastName = '권';
+v.formattedName = '권한비(BIBI)';
+v.organization = '';
+v.cellPhone = '010-7410-0030';
+v.email = '072hanvy@gmail.com';
+v.url = 'https://www.linkedin.com/in/hanbi-kwon/';
+v.note = 'VCF를 통해 추가된 연락처입니다.';
+
+// 줄바꿈을 CRLF로 통일 (BOM 넣지 않음: Android 호환성↑)
+const body = v.getFormattedString().replace(/\r?\n/g, '\r\n');
+fs.writeFileSync('./hanbi.vcf', body, { encoding: 'utf8' });
+
+console.log('wrote hanbi.vcf');
